6100d437b98ad7851edc97661d57e42552439b88
BrianShiau <brian-s@utexas.edu>
2016-10-31 22:19:23 -0500

    added segmented view controller view switching functionality

--- 1000 Cards/GameView.swift
+++ 1000 Cards/GameView.swift
@@ -6,85 +6,27 @@
 //  Copyright © 2016 EggLord. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
-class GameView: UIViewController, iCarouselDataSource, iCarouselDelegate
-{
-    var items: [Int] = []
-    @IBOutlet var carousel: iCarousel!
+class GameView: UIViewController {
+    @IBOutlet weak var containerViewGame: UIView!
+    @IBOutlet weak var containerViewHand: UIView!
     
-    override func awakeFromNib()
-    {
-        super.awakeFromNib()
-        for i in 0...10
-        {
-            items.append(i)
-        }
-    }
-    
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        carousel.type = .coverFlow2
     }
     
-    func numberOfItems(in carousel: iCarousel) -> Int
-    {
-        return items.count
-    }
-    
-    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView
-    {
-        var label: UILabel
-        var itemView: UIImageView
-        
-        //create new view if no view is available for recycling
-        if (view == nil)
-        {
-            //don't do anything specific to the index within
-            //this `if (view == nil) {...}` statement because the view will be
-            //recycled and used with other index values later
-            itemView = UIImageView(frame:CGRect(x:0, y:0, width:200, height:200))
-            itemView.image = UIImage(named: "damnapes.jpg")
-            itemView.contentMode = .center
-            
-            label = UILabel(frame:itemView.bounds)
-            label.backgroundColor = UIColor.clear
-            label.textAlignment = .center
-            label.tag = 1
-            label.contentMode = .top
-            itemView.addSubview(label)
+    @IBAction func showComponent(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.containerViewGame.alpha = 1
+                self.containerViewHand.alpha = 0
+            })
+        } else {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.containerViewGame.alpha = 0
+                self.containerViewHand.alpha = 1
+            })
         }
-        else
-        {
-            //get a reference to the label in the recycled view
-            itemView = view as! UIImageView;
-            label = itemView.viewWithTag(1) as! UILabel!
-        }
-        
-        //set item label
-        //remember to always set any properties of your carousel item
-        //views outside of the `if (view == nil) {...}` check otherwise
-        //you'll get weird issues with carousel item content appearing
-        //in the wrong place in the carousel
-        label.text = "\(items[index])"
-        
-        return itemView
     }
-    
-    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat
-    {
-        if (option == .spacing)
-        {
-            return value * 1.1
-        }
-        return value
-    }
-    
-    func carousel(_ carousel: iCarousel, didSelectItemAt index: Int) {
-        let vc : AnyObject! = self.storyboard!.instantiateViewController(withIdentifier: "cardViewController")
-        self.navigationController?.pushViewController(vc as! UIViewController, animated: true)
-    }
-    
 }
