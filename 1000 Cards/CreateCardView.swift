909146587e72aa98a5607d9feae0da8608d0f0cf
David Alexander <davidalexander@David-Alexanders-Computer.local>
2016-11-12 15:46:51 -0600

    fix select image in create card

--- 1000 Cards/CreateCardView.swift
+++ 1000 Cards/CreateCardView.swift
@@ -11,24 +11,26 @@ import UIKit
 
 class CreateNewCardView: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
-
+    //MARK: Properties
     @IBOutlet weak var titleTF: UITextField!
     @IBOutlet weak var descriptionTF: UITextView!
     @IBOutlet weak var newCardImageView: UIImageView!
+    let imagePicker = UIImagePickerController()
     var deck = PFObject(className: "Deck")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        let tapGesture = UITapGestureRecognizer(target: self, action: Selector(("imageTapped:")))
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(CreateNewCardView.imageTapped))
+        imagePicker.delegate = self
         // add it to the image view;
         newCardImageView.addGestureRecognizer(tapGesture)
         // make sure imageView can be interacted with by user
         newCardImageView.isUserInteractionEnabled = true
+        newCardImageView.image = UIImage(named: "defaultImage.jpg")
     }
-    
+
     @IBAction func createCardButton(_ sender: AnyObject) {
         //handle empty input
         if((titleTF.text?.isEmpty)!){
@@ -47,31 +49,55 @@ class CreateNewCardView: UIViewController, UINavigationControllerDelegate, UIIma
 
     }
     
-    func imageTapped(gesture: UIGestureRecognizer) {
+//    func imageTapped(gesture: UIGestureRecognizer) {
+    func imageTapped() {
         // if the tapped view is a UIImageView then set it to imageview
 //        if let imageView = gesture.view as? UIImageView {
 //
 //            //Here you can initiate your new ViewController
 //            
 //        }
-        let image = UIImagePickerController()
-        image.delegate = self
-        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
-        self.present(image, animated: true, completion: nil)
+//        let image = UIImagePickerController()
+//        image.delegate = self
+//        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+//        self.present(image, animated: true, completion: nil)
         
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
     }
     
-    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        let imgInfo: NSDictionary = info as NSDictionary
-        let img: UIImage = imgInfo.object(forKey: UIImagePickerControllerOriginalImage) as! UIImage
-        newCardImageView.image = img
-        self.dismiss(animated: true, completion: nil)
+//    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+//        let imgInfo: NSDictionary = info as NSDictionary
+//        let img: UIImage = imgInfo.object(forKey: UIImagePickerControllerOriginalImage) as! UIImage
+//        newCardImageView.image = img
+//        self.dismiss(animated: true, completion: nil)
+//    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            newCardImageView.contentMode = .scaleAspectFit
+            newCardImageView.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
     
     
     /*
