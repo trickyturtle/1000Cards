19cdd85b01f1e6f7eb840bc130c7635bea8321f5
David Alexander <dralexander08@gmail.com>
2016-11-15 22:46:45 -0600

    update Game class

--- 1000 Cards/Game.swift
+++ 1000 Cards/Game.swift
@@ -12,15 +12,36 @@ class Game: NSObject {
     var players:[String]
     var points:[Int]
     
-    //var played:[Card]
-    //var discard:[Card]
-    //var inPlay:[Card]
-    //var hand:[Card]
+    var played:[String]
+    var discard:[String]
+    var inPlayOn1:[String]
+    var inPlayOn2:[String]
+    var inPlayOn3:[String]
+    var inPlayOn4:[String]
+    var hand:[String]
+    var deck: [String]
     
     // MARK: Initialization
     
-    init(players: [String], points: [Int]) {
+    init(players: [String], theDeck: PFObject) {
         self.players = players
-        self.points = points
+        self.points = [0,0,0,0]
+        
+        //played is the vector of cards that are played "in general" (ie new rules, etc)
+        self.played = []
+        self.discard = []
+        self.inPlayOn1 = []
+        self.inPlayOn2 = []
+        self.inPlayOn3 = []
+        self.inPlayOn4 = []
+        self.hand = []
+        self.deck = []
+        let tempDeck: [String] = theDeck.object(forKey: "cards") as! [String]
+        
+        for i in tempDeck
+        {
+            self.deck.append(i)
+        }
+        
     }
 }
