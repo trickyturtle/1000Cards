33a30c179b1e7784e6262fb80c2d819c526d01b5
David Alexander <dralexander08@gmail.com>
2016-11-18 17:43:10 -0600

    add logic messaging to GameAction

--- 1000 Cards/GameAction.swift
+++ 1000 Cards/GameAction.swift
@@ -32,4 +32,48 @@ class GameAction{
             abort()
         }
     }
+    
+    static func createActionMessage(player: String, source: String, action: String, dest: String)->String{
+        let actionMessage = PFObject(className: "ActionMessage")
+        actionMessage.add(source, forKey: "source")
+        actionMessage.add(player, forKey: "player")
+        actionMessage.add(action, forKey: "action")
+        actionMessage.add(dest, forKey: "destination")
+        actionMessage.saveInBackground()
+        return actionMessage.objectId!
+    }
+    
+    static func formatAMForPrint(messageID: String)->String{
+        //TODO: error handle
+        let actionMessage = PFQuery.getObjectOfClass("ActionMessage", objectId: messageID)
+        //TODO: we probably expect a player ID and convert it into a username
+        let name:String = actionMessage.object(forKey: "player") as! String
+        let source:String = actionMessage.object(forKey: "source") as! String
+        let action:String = actionMessage.object(forKey: "action") as! String
+        let dest:String
+            = actionMessage.object(forKey: "dest") as! String
+        
+        if (action == "pop"){
+            if (source == "deck"){
+                return "\(name) drew a card"
+            }
+        }
+        else{
+            if (source == "hand"){
+                //TODO error handling
+                let cardTitle:String = (PFQuery.getObjectOfClass("Card", objectId: action).object(forKey: "title") as! String?)!
+                return "\(name) played \(cardTitle)"
+            }
+            else if (source == "inPlay" && dest == "discard"){
+                //TODO error handling
+                let cardTitle:String = (PFQuery.getObjectOfClass("Card", objectId: action).object(forKey: "title") as! String?)!
+                return "\(name) discarded \(cardTitle)"
+            }
+            else if (dest == "deck"){
+                return "\(name) added a card to the deck"
+            }
+        }
+
+        
+    }
 }
