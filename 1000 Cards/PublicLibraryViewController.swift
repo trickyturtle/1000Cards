2df36b1f4e6de40026e9d5b4a083f3338b008a6b
BrianShiau <brian-s@utexas.edu>
2016-11-20 17:16:22 -0600

    public library start

---
+++ 1000 Cards/PublicLibraryViewController.swift
@@ -0,0 +1,57 @@
+//
+//  PublicLibraryViewController.swift
+//  1000 Cards
+//
+//  Created by Brian Shiau on 11/20/16.
+//  Copyright © 2016 Isaac Garza. All rights reserved.
+//
+
+import UIKit
+
+class PublicLibraryViewController: UIViewController, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    var sampleLibrary = ["Sample Deck"]
+    @IBAction func createNewDeck(_ sender: AnyObject) {
+    }
+    
+    //set number of sections in the table view to 1
+    private func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //set number of rows in table view to be number of candidates
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sampleLibrary.count
+    }
+    
+    //what to display in the table: first name and last name and num votes for each candidate
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "publicLibraryCell", for: indexPath as IndexPath)
+        let row = indexPath.row
+        cell.textLabel?.text = sampleLibrary[row]
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
