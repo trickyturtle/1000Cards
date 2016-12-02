f11225f7349a36829bdd63d8be5a70b8837b1a97
David Alexander <dralexander08@gmail.com>
2016-12-01 18:03:05 -0600

    added player number getting in handview

--- 1000 Cards/HandViewController.swift
+++ 1000 Cards/HandViewController.swift
@@ -36,6 +36,9 @@ class HandViewController: GameCardCarouselView
                 controller.addAction(UIAlertAction(title: "OK",style: .default, handler: nil))
                 self.present(controller, animated: true, completion: nil)
             }else{
+                let controller = UIAlertController(title: "Card Drawn!", message: "It may take a minute for your hand to update, depending on your network connection", preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "OK",style: .default, handler: nil))
+                self.present(controller, animated: true, completion: nil)
                 let card = result[Int(arc4random_uniform(UInt32(numCards)))]
                 relation.remove(card)
                 relation = game.relation(forKey: deckTypeKey)
@@ -54,8 +57,45 @@ class HandViewController: GameCardCarouselView
         
     }
     
+    static func getPlayerHandNum(game: PFObject)->Int{
+        var i = 0
+        while i < 4{
+            if (PFUser.current()!.username! == game["player\(i)"]! as! String){
+                return i
+            }
+            i += 1
+        }
+        return 0
+    }
+    
     //TODO: set up a notification to handle this
     func playCard(){
+        var relation = game.relation(forKey: "")
+        let query = relation.query()
+        var result = [PFObject]()
+        do {
+            result = try query.findObjects()
+            let numCards = result.count
+            if numCards == 0{
+                let controller = UIAlertController(title: "Deck is empty!", message: "Create more cards!", preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "OK",style: .default, handler: nil))
+                self.present(controller, animated: true, completion: nil)
+            }else{
+                let controller = UIAlertController(title: "Card Drawn!", message: "It may take a minute for your hand to update, depending on your network connection", preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "OK",style: .default, handler: nil))
+                self.present(controller, animated: true, completion: nil)
+                let card = result[Int(arc4random_uniform(UInt32(numCards)))]
+                relation.remove(card)
+                relation = game.relation(forKey: deckTypeKey)
+                relation.add(card)
+                //TODO: this needs to happen more quickly, we need save() and error handling or an alert
+                game.saveEventually()
+            }
+            
+            
+        } catch {
+            print(error)
+        }
     }
     
     
